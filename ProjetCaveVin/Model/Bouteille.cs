dca18d6ac52af77d37f5689d3e1690cc57585409
YohanSTM-dev <yohan.saint-marc@hotmail.com>
2025-09-22 09:37:08 +0200

    initialisation du projet avec les dossier, gitignore et mvvm avec wpf

---
+++ ProjetCaveVin/Model/Bouteille.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetCaveVin.Model
+{
+    internal class Bouteille
+    {
+
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        
+        public int Millesime { get; set; }
+        public int Quantite { get; set; }
+
+    }
+}
