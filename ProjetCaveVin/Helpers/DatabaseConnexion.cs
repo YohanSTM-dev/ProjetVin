7cd535ff13a77c5e3c95831885f08edb44253400
YohanSTM-dev <yohan.saint-marc@hotmail.com>
2025-09-22 14:13:46 +0200

    projet avec fichier test

--- ProjetCaveVin/Helpers/DatabaseConnexion.cs
+++ ProjetCaveVin/Helpers/DatabaseConnexion.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using ProjetCaveVin.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjetCaveVin.Model;
+using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 namespace ProjetCaveVin.Helpers
 {
-    internal class DatabaseConnexion
+    public static class DatabaseConnexion
     {
+
+        public static bool TestConnexion(Connexion c)
+        {
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(c.ToString()))
+                {
+                    connection.Open();
+                    Console.Write("Connexion réussie");
+                    return true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write($"Erreur de connexion: {ex.Message}");
+                return false;
+            }
+        }
     }
 }
+     
--- ProjetCaveVin/Model/Connexion.cs
+++ ProjetCaveVin/Model/Connexion.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks
 
 namespace ProjetCaveVin.Model
 {
-    internal class Connexion
+    public class Connexion
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Port { get; set; } = 1433;
+        public string Host { get; set; }
+        public string Database { get; set; }
+        public Connexion() { }
+
+        public Connexion(string username, string password, int port, string host, string database)
+        {
+            Username = username;
+            Password = password;
+            Port = port;
+            Host = host;
+            Database = database;
+            SqlConnexion dl = new SqlConnexion(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Server={Host};Port={Port};Database={Database};User Id={Username};Password={Password};";
+        }
+
     }
 }
