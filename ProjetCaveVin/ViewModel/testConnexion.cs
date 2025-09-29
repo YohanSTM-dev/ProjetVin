d03208cdfd98fcea5070625aed7b1b790df9043a
YohanSTM-dev <yohan.saint-marc@hotmail.com>
2025-09-29 08:26:17 +0200

    ajout de la feature connxion-db avec tout fonctionnel pour une bonne gestion du projet

--- ProjetCaveVin/ViewModel/testConnexion.cs
+++ ProjetCaveVin/ViewModel/testConnexion.cs
@@ -25,7 +25,7 @@ namespace ProjetCaveVin.ViewModel
         {
             try
             {
-                string connectionString = @"Server=172.16.119.42\SQLEXPRESS02,1433;Database=restaurant;User Id=yohan;Password=1234;Encrypt=False;";
+                string connectionString = @"Server=172.16.119.41\SQLEXPRESS02,1433;Database=restaurant;User Id=yohan;Password=1234;Encrypt=False;";
 
                 var db = new DatabaseConnexion(connectionString);
 
